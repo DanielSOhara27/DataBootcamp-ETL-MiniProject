790424fd536cc2684073031ba405df93b6b8ad26
Rulo <reperezri88@gmail.com>
2019-02-23 10:08:16 -0600

    Load sql script, and first draft of python bd load

---
+++ Resources/MySQL/create_delitos_bd.sql
@@ -0,0 +1,20 @@
+-- Drops the programming_db if it already exists --
+DROP DATABASE IF EXISTS delitos_db;
+-- Create a database called programming_db --
+CREATE DATABASE delitos_db;
+
+use delitos_db;
+
+create table icecreamstore(
+	id int auto_increment NOT NULL,
+    Año int,
+    Mes varcharacter(20),
+    Alcaldia varcharacter(100),
+    Categoria_del_delito varcharacter(100),
+    Delito varcharacter(100),
+    Unidad_de_inestigacion varcharacter(20),
+    Fecha_inicio varchar(20),
+    Colonia varcharacter(100),
+    Geopoint varchar(25),
+    PRIMARY KEY(id)
+);
